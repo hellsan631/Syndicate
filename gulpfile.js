77d50a8cd2f3ea2b93fa002182ae9143e1617906
Mathew Kleppin <matkle414@gmail.com>
2015-11-11 22:17:09 -0600

    pre inject

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,106 @@
 // Plugins
 // --------------------------------------------------------------------
 
-var gulp    = require('gulp');
-var inject  = require('gulp-inject');
+var gulp        = require('gulp');
+var plumber     = require('gulp-plumber');
+var sass        = require('gulp-sass');
+var notify      = require('gulp-notify');
+var neat        = require('node-neat').includePaths;
+var path        = require('path');
+var inject      = require('gulp-inject');
+var bowerFiles  = require('main-bower-files');
+var es          = require('event-stream');
+var angularSort = require('gulp-angular-filesort');
+
+// --------------------------------------------------------------------
+// Error Handler
+// --------------------------------------------------------------------
+
+//the title and icon that will be used for the Grunt notifications
+var notifyInfo = {
+	title: 'Gulp',
+	icon: path.join(__dirname, 'gulp.png')
+};
+
+var onError = {
+  errorHandler: notify.onError({
+		title: notifyInfo.title,
+		icon: notifyInfo.icon,
+		message: 'Error: <%= error.message %>'
+	})
+};
 
 // --------------------------------------------------------------------
 // Variables
 // --------------------------------------------------------------------
 
+var vendorPath = 'client/src/vendor/';
+var sourcePath = 'client/src/';
+
 var config = {
+  sass: {
+    target: sourcePath + 'css',
+    source: sourcePath + 'css/scss/*.scss'
+  },
   inject: {
-    target: './client/src/index.html',
-    sources: [
-      
-    ]
+    target: sourcePath + 'index.html',
+    sources: {
+      vendor: {
+        css: [
+          vendorPath + 'Materialize/dist/css/materialize.css',
+        ],
+        js: [
+          vendorPath + 'jquery/dist/jquery.js',
+          vendorPath + 'Materialize/dist/js/materialize.js',
+          vendorPath + 'angular/angular.js',
+          vendorPath + 'angular-animate/angular-animate.js',
+          vendorPath + 'angular-materialize/src/angular-materialize.js',
+          vendorPath + 'angular-redactor/angular-redactor-9.x.js',
+          vendorPath + 'angular-resource/angular-resource.js',
+          vendorPath + 'angular-sanitize/angular-sanitize.js',
+          vendorPath + 'angular-ui-router/release/angular-ui-router.js'
+        ]
+      },
+      app: {
+        css: [
+          sourcePath + 'css/styles.css'
+        ],
+        js: [
+          sourcePath + 'app/*.js',
+          sourcePath + 'app/**/*.js',
+          sourcePath + 'app/**/**/*.js'
+        ]
+      }
+    }
   }
-}
+};
 
 // --------------------------------------------------------------------
 // Tasks
 // --------------------------------------------------------------------
 
 gulp.task('inject', function () {
-  var target  = gulp.src('./client/src/index.html');
-  // It's not necessary to read the files (will speed up things), we're only after their paths:
-  var sources = gulp.src(['./src/**/*.js', './src/**/*.css'], {read: false});
+  return gulp.src(inject.target)
+    .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
+    .pipe(inject(es.merge(
+      gulp.src(inject.sources.app.css),
+      gulp.src(inject.sources.app.js).pipe(angularSort())
+    )))
+    .pipe(gulp.dest(sourcePath));
+});
 
-  return target.pipe(inject(sources))
-    .pipe(gulp.dest('./src'));
+gulp.task('watch', ['watch-sass'], function() {
+
+  //watch .scss files
+  return gulp.watch(config.sass.source, ['watch-sass']);
+
+});
+
+gulp.task('watch-sass', function(){
+  return gulp.src(config.sass.source)
+    .pipe(plumber(onError))
+    .pipe(sass({
+      includePaths: ['styles'].concat(neat)
+    }))
+    .pipe(gulp.dest(config.sass.target));
 });
