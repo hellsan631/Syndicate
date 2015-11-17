bfaec88d7fb2f24ed8f8b20625d05e663e47eebc
Mathew Kleppin <matkle414@gmail.com>
2015-11-17 17:45:44 -0600

    finished forums

--- client/src/app/views/forum/topic.controller.js
+++ client/src/app/views/forum/topic.controller.js
@@ -5,12 +5,19 @@
     .module('app')
     .controller('TopicController', TopicController);
 
-  TopicController.$inject = ['$timeout', '$stateParams', '$localForage', 'createChangeStream', 'Topic'];
+  TopicController.$inject = [
+    '$rootScope', '$timeout', '$q',
+    '$stateParams', '$localForage',
+    'createChangeStream', 'Topic', 'Post'];
 
-  function TopicController($timeout, $stateParams, $localForage, createChangeStream, Topic){
+  function TopicController($rootScope, $timeout, $q, $stateParams, $localForage, createChangeStream, Topic, Post){
     var _this = this;
+    var _postModal = $('#NewPost');
 
-    _this.initialized = false;
+    _this.initialized   = false;
+    _this.newPost       = {};
+    _this.newPostModal  = newPostModal;
+    _this.createNewPost = createNewPost;
 
     syncPosts($stateParams.id);
     $localForage.setItem($stateParams.id, true);
@@ -25,6 +32,41 @@
       changes.on('data', function(msg) {
         getTopic(id);
       });
+    }
+
+    function newPostModal(quote, username){
+
+      if (quote) {
+        _this.newPost.content = '' +
+          '<p><blockquote>@' + username + '<br/>' + quote + '</blockquote></p><br/>';
+      }
+
+      _postModal.openModal();
+    }
+
+    function createNewPost(){
+      if(!_this.newPost.content || _this.newPost.content.length === 0) {
+        return errorMessage('No Post Content', 'You need to enter in some content to make a topic');
+      }
+
+      createPost(_this.topic)
+        .then(function(){
+          _postModal.closeModal();
+          _this.newPost = {};
+        });
+    }
+
+    function errorMessage(title, text) {
+      swal(title, text, 'error');
+
+      return $q(function(){return null;});
+    }
+
+    function createPost(topic){
+      _this.newPost.topicId = topic.id;
+      _this.newPost.memberId = $rootScope.currentUser.id;
+
+      return Post.create(_this.newPost).$promise;
     }
 
     function getTopic(id) {
