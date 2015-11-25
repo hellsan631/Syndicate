d6503a6c0ea84fc05636214f67303ec8b0aba1ec
Mathew Kleppin <matkle414@gmail.com>
2015-11-24 21:17:46 -0600

    added new admin area for editing members

--- client/src/app/views/admin/admin.controller.js
+++ client/src/app/views/admin/admin.controller.js
@@ -6,18 +6,68 @@
     .controller('AdminController', AdminController);
 
   AdminController.$inject = [
-    '$rootScope', '$q', '$localForage', 'Member', 'RandomWords'
+    '$scope', '$rootScope', '$timeout', '$q',
+    '$localForage',
+    'LoopBackAuth', 'createChangeStream', 'Member', 'RandomWords'
   ];
 
-  function AdminController($rootScope, $q, $localForage, Member, RandomWords){
+  function AdminController($scope, $rootScope, $timeout, $q, $localForage,
+    LoopBackAuth, createChangeStream, Member, RandomWords){
+
     var _this = this;
 
     _this.user = {
       password: RandomWords.password()
     };
+    _this.members = [];
 
     _this.password = password;
     _this.submitNewUser = submitNewUser;
+    _this.getMembers = getMembers;
+    _this.deleteMember = deleteMember;
+    _this.openEditModal = openEditModal;
+
+    getMembers();
+
+    function openEditModal(){
+      console.log('asd');
+    }
+
+    function deleteMember(memberId, event) {
+      event.stopPropagation();
+      console.log('ejllp');
+    }
+
+    function getMembers() {
+      Member.find().$promise
+        .then(filterNewResults);
+    }
+
+    function filterNewResults(results) {
+      results.forEach(function(result){
+        var dupe = false;
+
+        if (_this.members.length > 0){
+          _this.members.forEach(function(member){
+            if (result.id === member.id) {
+              dupe = true;
+            }
+          });
+        }
+
+        if (!dupe) {
+          _this.members.push(result);
+        }
+
+      });
+
+      if(!_this.initialized) {
+        $timeout(function(){
+          _this.initialized = true;
+        }, 100);
+      }
+
+    }
 
     function password() {
       _this.user.password = RandomWords.password();
